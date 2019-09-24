f47e9ece788fb9013aade670822cd356a8e87e3e
Umang Parmar <umangjparmar@gmail.com>
2019-09-24 21:15:50 +0530

    Add Function to calculate size of bit array from elements to track and acceptable false positive probability

--- bloomfilter.go
+++ bloomfilter.go
@@ -1,9 +1,18 @@
 package bloomfilter
 
+import (
+	"math"
+)
+
 // BloomFilter data struct
 type BloomFilter struct {
 	Size                               byte
 	HashFunctions                      byte
 	AcceptableFalsePositiveProbability float64
 	BitArray                           []byte
+}
+
+func getSizeOfBitArray(elements uint64, prob float64) byte {
+	return byte(math.Round(
+		-1 * float64(elements) * math.Log(prob) / math.Pow((math.Log(2)), 2)))
 }
