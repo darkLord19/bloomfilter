50f066dbfcaef605855418b9983ddd73447edcc2
Umang Parmar <umangjparmar@gmail.com>
2020-02-19 11:36:30 +0530

    NewBloomFilter accepts any hash type which implements hash64 interface

--- bloomfilter.go
+++ bloomfilter.go
@@ -25,7 +25,6 @@ package bloomfilter
 
 import (
 	"hash"
-	"hash/fnv"
 	"math"
 	"strconv"
 )
@@ -61,11 +60,12 @@ func (b *BloomFilter) getHash(seed int, key []byte) (uint64, error) {
 // NewBloomFilter returns pointer to newly created BloomFilter struct. It accepts two arguments.
 // 1st is number of elements you want to track
 // 2nd is acceptable false positive probability
-func NewBloomFilter(elements uint32, acceptableFalsePositiveProbability float64) *BloomFilter {
+// 3rd is the hash function you want to use
+func NewBloomFilter(elements uint32, acceptableFalsePositiveProbability float64, hash hash.Hash64) *BloomFilter {
 	size := getSizeOfBitArray(elements, acceptableFalsePositiveProbability)
 	hashFuncs := getOptimumNumOfHashFuncs(size, elements)
 
-	return &BloomFilter{size, make([]bool, size), hashFuncs, fnv.New64(), acceptableFalsePositiveProbability, 0}
+	return &BloomFilter{size, make([]bool, size), hashFuncs, hash, acceptableFalsePositiveProbability, 0}
 }
 
 // Add inserts new element in bloomfilter instance
